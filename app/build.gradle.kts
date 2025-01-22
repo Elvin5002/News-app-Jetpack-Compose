9af93645131c57b55567de00f6c7826560c16006
Elvin <114992009+Elvin5002@users.noreply.github.com>
2025-01-22 13:15:03 +0400

    - first commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,111 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    id ("kotlin-kapt")
+    id ("com.google.dagger.hilt.android")
+    id ("kotlin-parcelize")
+}
+
+android {
+    namespace = "com.example.newsapp"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.newsapp"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+
+    //Splash Api
+    implementation(libs.androidx.core.splashscreen)
+
+    //Compose Navigation
+    implementation("androidx.navigation:navigation-compose:2.8.5")
+
+    //Dagger Hilt
+    implementation ("com.google.dagger:hilt-android:2.47")
+    kapt ("com.google.dagger:hilt-compiler:2.47")
+    implementation ("androidx.hilt:hilt-navigation-compose:1.2.0")
+
+
+    //Retrofit
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    //Coil
+    implementation("io.coil-kt:coil-compose:2.4.0")
+
+    //Datastore
+    implementation ("androidx.datastore:datastore-preferences:1.1.2")
+
+    //Compose Foundation
+    implementation ("androidx.compose.foundation:foundation:1.7.6")
+
+    //Accompanist
+    implementation ("com.google.accompanist:accompanist-systemuicontroller:0.32.0")
+
+    //Paging 3
+    val pagingVersion = "3.1.1"
+    implementation ("androidx.paging:paging-runtime:$pagingVersion")
+    implementation ("androidx.paging:paging-compose:3.3.5")
+
+    //Room
+    val roomVersion = "2.6.1"
+    implementation (libs.androidx.room.runtime)
+    kapt ("androidx.room:room-compiler:$roomVersion")
+    implementation(libs.androidx.room.ktx)
+}
